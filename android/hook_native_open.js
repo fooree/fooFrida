e4412aacbc14abf46eca565f784e7cd730359a4b
Fu <gp490609578@163.com>
2018-07-27 10:33:25 +0800

    hook native function

--- android/hook_native_open.js
+++ android/hook_native_open.js
@@ -1,26 +1,29 @@
 // UTF-8
 'use strict'
 
-// 查找libc.so模块中的系统函数open()在内存中的地址
-// 关于open()该函数的标准文档参考：http://man7.org/linux/man-pages/man2/open.2.html
-target_address = Module.findExportByName("libc.so", "open");
+// Interceptor.attach(target, callbacks)：
+// 功能是hook指定的native函数。
+// target表示目标函数的内存地址，callbacks表示目标函数执行时的回调对象，也就是注入的功能代码
 
-// 回调对象
-callback = {
-    // onEnter()函数在目标方法被执行前执行
-    // args表示目标方法的参数，但是args是一个JavaScript对象数组，需要转换为C/C++数据类型
+// target = Module.findExportByName(module, function):
+// 功能是查找指定模块中的目标函数(变量)的内存地址。
+// 此处的例子是查找libc.so模块中的系统函数open()的内存地址
+// 关于open()该函数的标准文档：http://man7.org/linux/man-pages/man2/open.2.html
+
+// 执行以下代码后，当target处的代码被执行时，Frida会调用callback这个回调对象
+Interceptor.attach(Module.findExportByName("libc.so", "open"), {
+    // onEnter()函数在目标函数被执行前执行
+    // args表示目标函数的参数，但是args是一个JavaScript对象数组，需要转换为C/C++数据类型
     onEnter: function (args) {
+        // 这里是自定义的功能
         // Memory.readCString(args[0])表示将第一个参数转换为C语言的字符串类型
         console.log("[+]  open file => " + Memory.readCString(args[0]))
     },
-    // onLeave()函数在目标方法被执行后执行
-    // retval表示目标方法的返回值
+    // onLeave()函数在目标函数被执行后执行
+    // retval表示目标函数的返回值
     onLeave: function (retval) {
+        // 这里是自定义的功能
         // toInt32()表示将一个JavaScript数据对象转换为C语言的int数据类型
         console.log("[+]  open return: " + retval.toInt32())
     }
-}
-// 执行HOOK
-// target_address参数表示目标函数的内存地址
-// 执行以下代码后，当target_address内存地址处的代码被执行时,Frida会调用callback这个回调对象
-Interceptor.attach(target_address, callback);
+});
